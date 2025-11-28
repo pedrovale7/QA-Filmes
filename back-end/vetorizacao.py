e873fdb9fe41bb4e7d260171baf6e5f7357c5fb2
pedrovale7 <pedrovaleprod@gmail.com>
2025-11-28 10:26:34 -0300

    "Ajustes sobre pacotes e Criação do makefile para setup"

--- back-end/vetorizacao.py
+++ back-end/vetorizacao.py
@@ -4,8 +4,11 @@ import os
 from fastembed import TextEmbedding
 
 os.environ["HF_HUB_DISABLE_SYMLINKS"] = "1"
+dir = os.path.dirname(os.path.abspath(__file__))
+path_csv = os.path.join(dir, 'pre_processed_movies.csv')
+path_npy = os.path.join(dir, 'vetores_filmes.npy')
+df = pd.read_csv(path_csv)
 
-df = pd.read_csv('pre_processed_movies.csv')
 def vetorizar_texto():
     vetores = []
     model = TextEmbedding('BAAI/bge-small-en-v1.5')
@@ -19,17 +22,17 @@ def vetorizar_texto():
         else:
             tags = ""
 
-        texto = f"Título: {titulo}\nGênero: {genero}\nTags: {tags}"
+        texto = f"Título: {titulo}\n Gênero: {genero}\n Tags: {tags}"
         vetores = list(model.embed([texto]))
     
-    np.save('back-end/vetores_filmes.npy', vetores)
+    np.save(path_npy, vetores)
 
 
 def busca_vetorial(texto_busca, top_k=5):
     model = TextEmbedding('BAAI/bge-small-en-v1.5')
     vetor_busca = list(model.embed([texto_busca]))[0]
 
-    vetores_filmes = np.load('vetores_filmes.npy')
+    vetores_filmes = np.load(path_npy)
 
     similaridades = np.dot(vetores_filmes, vetor_busca) / (np.linalg.norm(vetores_filmes, axis=1) * np.linalg.norm(vetor_busca))
 
@@ -39,3 +42,5 @@ def busca_vetorial(texto_busca, top_k=5):
     resultados['similaridade'] = similaridades[indices_top_k]
 
     return resultados.to_dict(orient='records')
+
+vetorizar_texto()
